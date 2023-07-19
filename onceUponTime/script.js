d2fefa47b23eba0c6d60a4bf6f9febc56debb619
Clément Badin <clement@MacBook-Air-de-Clement.local>
2023-07-19 12:29:45 +0200

    [CREATE] Videos fonctionnelles page features [UPDATE] display features ok [PROBLEM] changing between features, documentary and more, break display

--- onceUponTime/script.js
+++ onceUponTime/script.js
@@ -141,12 +141,189 @@ moreAccess.addEventListener("click", () => {
 
 // let survivantsTrailer = "https://www.youtube.com/watch?v=vY1kqd1Dk5Q";
 
+//
+
+//==================================//
+
+// ----------- TRAILERS ----------- //
+
+// 1 - Trailer for FEATURES
+
 const survivorsVideo =
   "https://res.cloudinary.com/dzq7b6tce/video/upload/v1689685868/Projet_SoundDesigner/OnceUponATime/Bande-annonce/LES_SURVIVANTS_Bande-annonce_nuhp0u.mp4";
 
-// survivors.addEventListener("click", () => {
-//   workContainer.innerHTML += `<video class="featureVideo" src=${survivorsVideo} controls autoplay type="video.mp4"></video>`;
-// });
-// const test = () => {
-//   survivorsImg.style.background = "red";
+const teddyVideo =
+  "https://res.cloudinary.com/dzq7b6tce/video/upload/v1689719956/Projet_SoundDesigner/OnceUponATime/Bande-annonce/Teddy_Trailer_hrizte.mp4";
+
+const juniorsVideo =
+  "https://res.cloudinary.com/dzq7b6tce/video/upload/v1689720351/Projet_SoundDesigner/OnceUponATime/Bande-annonce/Junior_Trailer_sy7yui.mp4";
+
+const sharkVideo =
+  "https://res.cloudinary.com/dzq7b6tce/video/upload/v1689720213/Projet_SoundDesigner/OnceUponATime/Bande-annonce/ADR_Trailer_eva9l8.mp4";
+
+const brotherVideo =
+  "https://res.cloudinary.com/dzq7b6tce/video/upload/v1689720589/Projet_SoundDesigner/OnceUponATime/Bande-annonce/Mon_fr%C3%A8re_trailer_ovgs3m.mp4";
+
+const micVideo =
+  "https://res.cloudinary.com/dzq7b6tce/video/upload/v1689720758/Projet_SoundDesigner/OnceUponATime/Bande-annonce/Made_In_China_Trailer_msgl3a.mp4";
+
+const survivorsTrailer = () => {
+  survivors.addEventListener("click", () => {
+    workContainer.innerHTML += `<div id="featureVideo">
+  <video src=${survivorsVideo} controls autoplay type="video.mp4"></video>
+  <button id="featureVideoButton" > <i class="fa-regular fa-circle-xmark"></i></button>
+  </div>`;
+    featureVideoButton.addEventListener("click", () => {
+      featureVideo.remove();
+      survivorsTrailer();
+      juniorsTrailer();
+      brotherTrailer();
+      teddyTrailer();
+      micTrailer();
+      sharkTrailer();
+    });
+  });
+};
+survivorsTrailer();
+// Pour que la video puisse être rejouée au click après l'avoir fermée en appuyant sur la croix, ce qui déclenche le remove(), il faut ensuite la rappeler, pour réactiver la fonction qui permettra de déclencher la vidéo au click.
+
+const juniorsTrailer = () => {
+  juniors.addEventListener("click", () => {
+    workContainer.innerHTML += `<div id="featureVideo">
+  <video src=${juniorsVideo} controls autoplay type="video.mp4"></video>
+  <button id="featureVideoButton" > <i class="fa-regular fa-circle-xmark"></i></button>
+  </div>`;
+    featureVideoButton.addEventListener("click", () => {
+      featureVideo.remove();
+      juniorsTrailer();
+      survivorsTrailer();
+      brotherTrailer();
+      teddyTrailer();
+      micTrailer();
+      sharkTrailer();
+    });
+  });
+};
+juniorsTrailer();
+
+const brotherTrailer = () => {
+  brother.addEventListener("click", () => {
+    workContainer.innerHTML += `<div id="featureVideo">
+  <video src=${brotherVideo} controls autoplay type="video.mp4"></video>
+  <button id="featureVideoButton" > <i class="fa-regular fa-circle-xmark"></i></button>
+  </div>`;
+    featureVideoButton.addEventListener("click", () => {
+      featureVideo.remove();
+      juniorsTrailer();
+      survivorsTrailer();
+      brotherTrailer();
+      teddyTrailer();
+      micTrailer();
+      sharkTrailer();
+    });
+  });
+};
+brotherTrailer();
+
+const teddyTrailer = () => {
+  teddy.addEventListener("click", () => {
+    workContainer.innerHTML += `<div id="featureVideo">
+  <video src=${teddyVideo} controls autoplay type="video.mp4"></video>
+  <button id="featureVideoButton" > <i class="fa-regular fa-circle-xmark"></i></button>
+  </div>`;
+    featureVideoButton.addEventListener("click", () => {
+      featureVideo.remove();
+      juniorsTrailer();
+      survivorsTrailer();
+      brotherTrailer();
+      teddyTrailer();
+      micTrailer();
+      sharkTrailer();
+    });
+  });
+};
+teddyTrailer();
+
+const micTrailer = () => {
+  mic.addEventListener("click", () => {
+    workContainer.innerHTML += `<div id="featureVideo">
+  <video src=${micVideo} controls autoplay type="video.mp4"></video>
+  <button id="featureVideoButton" > <i class="fa-regular fa-circle-xmark"></i></button>
+  </div>`;
+    featureVideoButton.addEventListener("click", () => {
+      featureVideo.remove();
+      juniorsTrailer();
+      survivorsTrailer();
+      brotherTrailer();
+      teddyTrailer();
+      micTrailer();
+      sharkTrailer();
+    });
+  });
+};
+micTrailer();
+
+const sharkTrailer = () => {
+  shark.addEventListener("click", () => {
+    workContainer.innerHTML += `<div id="featureVideo">
+  <video src=${sharkVideo} controls autoplay type="video.mp4"></video>
+  <button id="featureVideoButton" > <i class="fa-regular fa-circle-xmark"></i></button>
+  </div>`;
+    featureVideoButton.addEventListener("click", () => {
+      featureVideo.remove();
+      juniorsTrailer();
+      survivorsTrailer();
+      brotherTrailer();
+      teddyTrailer();
+      micTrailer();
+      sharkTrailer();
+    });
+  });
+};
+sharkTrailer();
+
+// 2 - Trailers for Documentary
+
+const raisingVideo =
+  "https://res.cloudinary.com/dzq7b6tce/video/upload/v1689724138/Projet_SoundDesigner/OnceUponATime/Bande-annonce/Raising_Trailer_vnzhpz.mp4";
+
+const nekfeuVideo =
+  "https://res.cloudinary.com/dzq7b6tce/video/upload/v1689725040/Projet_SoundDesigner/OnceUponATime/Bande-annonce/Nekfeu_Trailer_p1tsal.mp4";
+
+// const raisingTrailer = () => {
+//   raising.addEventListener("click", () => {
+//     workContainer.innerHTML += `<div id="featureVideo">
+//   <video src=${raisingVideo} controls autoplay type="video.mp4"></video>
+//   <button id="featureVideoButton" > <i class="fa-regular fa-circle-xmark"></i></button>
+//   </div>`;
+//     featureVideoButton.addEventListener("click", () => {
+//       featureVideo.remove();
+//       juniorsTrailer();
+//       survivorsTrailer();
+//       brotherTrailer();
+//       teddyTrailer();
+//       micTrailer();
+//       sharkTrailer();
+//       raisingTrailer();
+//     });
+//   });
 // };
+// raisingTrailer();
+
+raising.addEventListener("click", () => {
+  console.log("ok");
+});
+
+// 3 - Trailers for More
+
+const maronaVideo =
+  "https://res.cloudinary.com/dzq7b6tce/video/upload/v1689725325/Projet_SoundDesigner/OnceUponATime/Bande-annonce/Marona_trailer_phlf5u.mp4";
+
+const tankVideo =
+  "https://res.cloudinary.com/dzq7b6tce/video/upload/v1689725435/Projet_SoundDesigner/OnceUponATime/Bande-annonce/TANK2_trailer_ni3chl.mp4";
+
+const theWayVideo =
+  "https://res.cloudinary.com/dzq7b6tce/video/upload/v1689725595/Projet_SoundDesigner/OnceUponATime/Bande-annonce/TheWay_Trailer_pjznyv.mp4";
+
+const novarosaVideo =
+  "https://res.cloudinary.com/dzq7b6tce/video/upload/v1689725647/Projet_SoundDesigner/OnceUponATime/Bande-annonce/Pub_Novarosa_AdobeExpress_s3xior.mp4";
